cf296a9fe045323a9280d56a40e6b8746852b40a
NuriaFL <104454234+NuriaFL@users.noreply.github.com>
2022-05-05 17:24:25 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "snowflake_database" "demo_db2" {
 }
 
 resource "snowflake_schema" "demo_schema" {
-  database = snowflake_database.demo_db.name
+  database = snowflake_database.demo_db2.name
   name     = "DEMO_SCHEMA"
   comment  = "Schema for Snowflake Terraform demo"
 }
